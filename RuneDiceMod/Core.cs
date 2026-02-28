0e2dff8be5e875737cf5e71cb978af2bae2b8c27
Alex Bolduc <axbolduc@gmail.com>
2026-02-28 14:34:17 -0500

    Add project files.

---
+++ RuneDiceMod/Core.cs
@@ -0,0 +1,19 @@
+﻿using Il2CppRogue2048;
+using MelonLoader;
+using System.Reflection;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(RuneDiceMod.Core), "RuneDiceMod", "1.0.0", "Alex", null)]
+[assembly: MelonGame("Smart Raven Studio", "Rune Dice Demo")]
+
+namespace RuneDiceMod
+{
+    public class Core : MelonMod
+    {
+
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
---
+++ RuneDiceMod/Patches/GameSpeedRepoPatch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using Il2CppRogue2048;
+using Il2CppSystem.Dynamic.Utils;
+using MelonLoader;
+
+namespace RuneDiceMod.Patches
+{
+    [HarmonyLib.HarmonyPatch(typeof(GameSpeedService), "Apply")]
+    internal class GameSpeedRepoPatch
+    {
+
+        static float[] GameSpeeds = new float[] { 1.0f, 1.5f, 2.0f, 2.5f, 3.0f, 3.5f, 4.0f, 4.5f, 5.0f, };
+        static float[] DiceSpeeds = new float[] { 1.0f, 1.5f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, 2.0f, };
+
+        private static void Postfix(ref GameSpeedService __instance)
+        {
+            Type GameSpeedServicetype = typeof(GameSpeedService);
+            PropertyInfo repositoryField = GameSpeedServicetype.GetProperty("_repository");
+
+            GameSpeedsRepository repo = (GameSpeedsRepository)repositoryField.GetValue(__instance);
+
+            GameSpeedConfig config = repo._configs.ElementAt(0);
+
+            if (!config.CombatSpeedMultipliers.Length.Equals(GameSpeeds.Length))
+            {
+                Melon<Core>.Logger.Msg("Updaing default game speeds");
+                config.DiceThrowingSpeedMultipliers = new Il2CppStructArray<float>(DiceSpeeds);
+                config.UnitsSpeedMultipliers = new Il2CppStructArray<float>(GameSpeeds);
+                config.DisplaySpeedMultipliers = new Il2CppStructArray<float>(GameSpeeds);
+                config.CombatSpeedMultipliers = new Il2CppStructArray<float>(GameSpeeds);
+            }
+        }
+    }
+}
